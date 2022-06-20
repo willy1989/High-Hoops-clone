9b6ae79afd5794f88606d88c6030e2c266f143cc
Willy <willy.winelover@gmail.com>
2022-06-20 18:00:09 +0200

    First draft. Managed to automate, but this needs to be optimized.

--- Assets/Scripts/Ball/BallNavigationWaypoint.cs
+++ Assets/Scripts/Ball/BallNavigationWaypoint.cs
@@ -8,9 +8,8 @@ public class BallNavigationWaypoint : MonoBehaviour
 
     public float BounceApex => bounceApex;
 
-    [SerializeField] private BallNavigationWaypoint nextBounceBlock;
+    [SerializeField] public BallNavigationWaypoint NextBounceBlock;
 
-    public BallNavigationWaypoint NextBounceBlock => nextBounceBlock;
 
     private BoxCollider boxCollider;
 
--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -8,7 +8,9 @@ public class Level : MonoBehaviour
 
     [SerializeField] private BallNavigationWaypoint secondWayPoint;
 
-    [SerializeField] private BlockPositionSetterGroup[] blockPositionSetterGroups;
+    private BlockPositionSetterGroup[] blockPositionSetterGroups;
+
+    [SerializeField] private BallNavigationWaypoint[][] ballNavigationWaypoints;
 
     public BallNavigationWaypoint FirstWayPoint => firstWayPoint;
     public BallNavigationWaypoint SecondWayPoint => secondWayPoint;
@@ -18,5 +20,43 @@ public class Level : MonoBehaviour
     private void Awake()
     {
         blockPositionSetterGroups = GetComponentsInChildren<BlockPositionSetterGroup>();
+
+        ballNavigationWaypoints = new BallNavigationWaypoint[blockPositionSetterGroups.Length][];
+
+        AssignNextWaypoints();
+    }
+
+    private void AssignNextWaypoints()
+    {
+        for(int i = 0; i < blockPositionSetterGroups.Length; i++)
+        {
+            ballNavigationWaypoints[i] = blockPositionSetterGroups[i].GetComponentsInChildren<BallNavigationWaypoint>();
+        }
+
+
+        for(int x = 0; x < ballNavigationWaypoints.Length-1; x++)
+        {
+            BallNavigationWaypoint[] currentGroup = ballNavigationWaypoints[x];
+            BallNavigationWaypoint[] nextGroup = ballNavigationWaypoints[x+1];
+
+            for (int y = 0; y < currentGroup.Length; y++)
+            {
+                BallNavigationWaypoint current = currentGroup[y];
+
+                BallColor currentBallColor = current.gameObject.GetComponent<ColorBlock>().BallColor;
+
+                for (int z = 0; z < nextGroup.Length; z++)
+                {
+                    BallNavigationWaypoint next = nextGroup[z];
+
+                    BallColor nextBallColor = next.gameObject.GetComponent<ColorBlock>().BallColor;
+
+                    if(nextBallColor == currentBallColor || next.CompareTag(Constants.LevelEnd_Tag) == true)
+                    {
+                        current.NextBounceBlock = next;
+                    }
+                }
+            }
+        }
     }
 }
